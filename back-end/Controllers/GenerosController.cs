e075ef144ad238fa755b42e14a623c78917ccdb1
Constanza <cxnstanzagamarra@gmail.com>
2022-06-26 17:44:44 -0300

    Iniciacion de proyecto

---
+++ back-end/Controllers/GenerosController.cs
@@ -0,0 +1,15 @@
+﻿using back_end.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    public class GenerosController
+    {
+        private readonly IRepoEnMemoria repositorio;
+        public GenerosController(IRepoEnMemoria repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+    }
+}
---
+++ back-end/Repositorio/RepoEnMemoria.cs
@@ -0,0 +1,23 @@
+﻿using back_end.Entidades;
+
+namespace back_end.Repositorio
+{
+    public class RepoEnMemoria
+    {
+        private List<Genero> _generos;
+
+        public RepoEnMemoria()
+        {
+            _generos = new List<Genero>()
+            {
+                new Genero(){ Id= 1 ,Nombre="Comedia"},
+                new Genero(){ Id= 2 ,Nombre="Accion"},
+            };
+        }
+
+    public List<Genero> ObtenerTodos()
+        {
+            return _generos;
+        }
+    }
+}
